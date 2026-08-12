e2d7bb1bb5f1952e1f8fb7c2f3a0581867175450
SHIVRAJ.A.MOHITE <154596032+Shivrajmohite7@users.noreply.github.com>
2026-08-12 13:52:29 +0530

    Add files via upload

---
+++ FBS/CORE PYTHON/DEMOS/FUNCTION/function5.py
@@ -0,0 +1,12 @@
+# with passing parameter(input) with returning value(rv)
+
+def addition(num1,num2):
+    add=num1+num2
+    return add
+
+
+num1=int(input("enter num1:"))
+num2=int(input("enter num1:"))
+
+res=addition(num1,num2)
+print("additon",res)
