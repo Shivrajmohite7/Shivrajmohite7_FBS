24d585a85b271edc4080840fc6026a665d440c98
SHIVRAJ.A.MOHITE <154596032+Shivrajmohite7@users.noreply.github.com>
2026-08-26 00:18:44 +0530

    Add files via upload

--- FBS/CORE PYTHON/DEMOS/BRANCHING/if_else_ladder.py
+++ FBS/CORE PYTHON/DEMOS/BRANCHING/if_else_ladder.py
@@ -1,190 +1,189 @@
-#  # 1.POSITIVE NEGATIVE NEUTRAL
+ # 1.POSITIVE NEGATIVE NEUTRAL
 
-# num=int(input("enter number:"))
-# if(num==0):{
-#     print("number is neutral")
-# }
-# elif(num>0):
-#     print("number is positive")
+num=int(input("enter number:"))
+if(num==0):{
+    print("number is neutral")
+}
+elif(num>0):
+    print("number is positive")
 
-# else:{
-#     print("number is negative")
-# }
+else:{
+    print("number is negative")
+}
 
-#2. NUMBER IN RANGE OF 0-50-100-150-250
+# 2. NUMBER IN RANGE OF 0-50-100-150-250
 
-# num=int(input("enter number:"))
+num=int(input("enter number:"))
 
-# if(num<=0):
-#     print("less than 0")
-# elif(num<=50):
-#     print("range is 0 to 50")
-# elif(num<=100):
-#     print("range is 50 to 100")
-# elif(num<=150):
-#     print("range is 100 to 150")
-# elif(num<=250):
-#     print("range is 150 to 250")
+if(num<=0):
+    print("less than 0")
+elif(num<=50):
+    print("range is 0 to 50")
+elif(num<=100):
+    print("range is 50 to 100")
+elif(num<=150):
+    print("range is 100 to 150")
+elif(num<=250):
+    print("range is 150 to 250")
 
-# else:print("big than 250")
+else:print("big than 250")
 
-# input("enter")
+input("enter")
 
-#3. GRADE DISTRIBUTION
+# 3. GRADE DISTRIBUTION
 
-# grade=int(input("Enter Grade:"))
+grade=int(input("Enter Grade:"))
 
-# if(grade<=0):
-#     print("grade is less than 0 so no use")
+if(grade<=0):
+    print("grade is less than 0 so no use")
 
-# elif(grade<=59):
-#     print("GRADE F")
+elif(grade<=59):
+    print("GRADE F")
 
-# elif(grade<=69):
-#     print("GRADE D")
+elif(grade<=69):
+    print("GRADE D")
 
-# elif(grade<=79):
-#     print("GRADE C")
+elif(grade<=79):
+    print("GRADE C")
 
-# elif(grade<=89):
-#     print("GRADE B")
+elif(grade<=89):
+    print("GRADE B")
 
-# elif(grade<=100):
-#     print("GRADE A")
+elif(grade<=100):
+    print("GRADE A")
 
-# input("enter")
+input("enter")
 
 # 4.AGE DECIDING WHETHER ADULD SENIORCITIZEN
 
-# num=int(input("Enter Age:"))
-# if(num<=0):
-#     print("INVALID AGE")
-# elif(num<=12):
-#     print("CHILD")
-# elif(num<=19):
-#     print("TEEN")
-# elif(num<=59):
-#     print("ADULT")
-# else:
-#     print("SENIOR CITIZEN")
-# input("ENTER")
+num=int(input("Enter Age:"))
+if(num<=0):
+    print("INVALID AGE")
+elif(num<=12):
+    print("CHILD")
+elif(num<=19):
+    print("TEEN")
+elif(num<=59):
+    print("ADULT")
+else:
+    print("SENIOR CITIZEN")
+input("ENTER")
 
-# ODD EVEN
+# 5. ODD EVEN
 
-# 5.num=int(input("Enter number:"))
+num=int(input("Enter number:"))
 
-# if(num<=0):
-#     print("zero")
-# elif(num%2==0):
-#     print("even")
-# else:
-#     print("odd")
-# input("Enter")
+if(num<=0):
+    print("zero")
+elif(num%2==0):
+    print("even")
+else:
+    print("odd")
+input("Enter")
 
 # 6.LIGHT BILL
 
-# bill=int(input("Enter bill:"))
+bill=int(input("Enter bill:"))
 
-# if(bill<=0):
-#     print("not valid")
-# elif(bill<=100):
-#     print("5 rs unit",bill*5)
-# elif(bill<=200):
-#     print("5 rs unit",bill*8)
-# elif(bill<=300):
-#     print("10 rs unit",bill*10)
-# else:
-#     print("12 rs ",bill*12)
+if(bill<=0):
+    print("not valid")
+elif(bill<=100):
+    print("5 rs unit",bill*5)
+elif(bill<=200):
+    print("5 rs unit",bill*8)
+elif(bill<=300):
+    print("10 rs unit",bill*10)
+else:
+    print("12 rs ",bill*12)
 
-# input('enter')
+input('enter')
 
-#7.NUMBER BIGGER IN THREE NUMBERS
+# 7.NUMBER BIGGER IN THREE NUMBERS
 
-# number=int(input("Enter number:"))
-# if(number<=0):{
-#     print("Not Valid")
-# }
-# elif(number<=20):{
-#     print(f"{number},Number is small")
-# }
-# elif(number<=50):
-#     print(f"{number},Number is big than 0-20")
+number=int(input("Enter number:"))
+if(number<=0):{
+    print("Not Valid")
+}
+elif(number<=20):{
+    print(f"{number},Number is small")
+}
+elif(number<=50):
+    print(f"{number},Number is big than 0-20")
 
-# elif(number<=80):
-#     print(f"{number},Number is bigger 0-50")
+elif(number<=80):
+    print(f"{number},Number is bigger 0-50")
 
-# elif(number<=100):
-#     print(f"{number},Number is biggest than 0-80")
+elif(number<=100):
+    print(f"{number},Number is biggest than 0-80")
 
-# else:
-#     print("enter number upto 100 only")
+else:
+    print("enter number upto 100 only")
 
-# input("enter")
+input("enter")
 
 # 8.DAY IN WEEK
 
-# digit=int(input("Enter Digit:"))
+digit=int(input("Enter Digit:"))
 
-# if(digit<=0):{
-#     print("Digit is invalid")
-# }
-# elif(digit==1):{
-#     print("It is MONDAY")
-# }
-# elif(digit==2):{
-#     print("It is TUESDAY")
-# }
-# elif(digit==3):{
-#     print("It is WEDNESDAY")
-# }
-# elif(digit==4):{
-#     print("It is THURSDAY")
-# }
-# elif(digit==5):{
-#     print("It is FRIDAY")
-# }
-# elif(digit==6):{
-#     print("It is SATURDAY")
-# }
-# elif(digit==7):{
-#     print("It is SUNDAY")
-# }  
-# else:
-#     print("INVALID")
-# input("enter")
+if(digit<=0):{
+    print("Digit is invalid")
+}
+elif(digit==1):{
+    print("It is MONDAY")
+}
+elif(digit==2):{
+    print("It is TUESDAY")
+}
+elif(digit==3):{
+    print("It is WEDNESDAY")
+}
+elif(digit==4):{
+    print("It is THURSDAY")
+}
+elif(digit==5):{
+    print("It is FRIDAY")
+}
+elif(digit==6):{
+    print("It is SATURDAY")
+}
+elif(digit==7):{
+    print("It is SUNDAY")
+}  
+else:
+    print("INVALID")
+input("enter")
 
-# BMI CATEGORY
+# 9.BMI CATEGORY
+Bmi=float(int(input("Enter BMI:")))
 
-#9. Bmi=float(int(input("Enter BMI:")))
+if(Bmi<=0):{
+    print("VALUE NOT EXISTS.")
+}
+elif(Bmi<=18.5):{
+    print("UNDERWEIGHT")
+}
+elif(Bmi<=25):{
+    print("NORMAL")
+}
+elif(Bmi<=30):{
+    print("OVERWEIGHT")
+}
+else:{
+    print("OBESE")
+}
 
-# if(Bmi<=0):{
-#     print("VALUE NOT EXISTS.")
-# }
-# elif(Bmi<=18.5):{
-#     print("UNDERWEIGHT")
-# }
-# elif(Bmi<=25):{
-#     print("NORMAL")
-# }
-# elif(Bmi<=30):{
-#     print("OVERWEIGHT")
-# }
-# else:{
-#     print("OBESE")
-# }
-
-# input("enter")
+input("enter")
 
 # 10.DECIDING WHETHER CHARACTER IS UPPERCASE LOWERCASE OR INVALID
 
-# char=(input("ENTER SINGLE CHAR:"))
-# if(char>='A' and char<='Z'):{
-#     print("UPPERCASE")
-# }
-# elif(char>='a' and char<='z'):{
-#     print('lowercase')
-# }
-# else:{
-#    print("it is symbol,number or else it is invalid")
-# }
-# input("enter")
+char=(input("ENTER SINGLE CHAR:"))
+if(char>='A' and char<='Z'):{
+    print("UPPERCASE")
+}
+elif(char>='a' and char<='z'):{
+    print('lowercase')
+}
+else:{
+   print("it is symbol,number or else it is invalid")
+}
+input("enter")
