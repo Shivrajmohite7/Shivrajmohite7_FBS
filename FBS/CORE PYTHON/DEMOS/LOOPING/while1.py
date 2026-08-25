f8a78bca2a12ebf0abb11113e6dbdf9c2f0439f9
SHIVRAJ.A.MOHITE <154596032+Shivrajmohite7@users.noreply.github.com>
2026-08-26 00:26:44 +0530

    Add files via upload

--- FBS/CORE PYTHON/DEMOS/LOOPING/while1.py
+++ FBS/CORE PYTHON/DEMOS/LOOPING/while1.py
@@ -1,6 +1,6 @@
-# i=0     #----variable intialization
-# while(i<5): #----variable used in condition
-#     print("ram")
-#     i+=1   #----i=i+1 variable value changed
+i=0     #----variable intialization
+while(i<5): #----variable used in condition
+    print("ram")
+    i+=1   #----i=i+1 variable value changed
 
-# input("enter")
+input("enter")
--- FBS/CORE PYTHON/DEMOS/LOOPING/while2.py
+++ FBS/CORE PYTHON/DEMOS/LOOPING/while2.py
@@ -1,13 +1,13 @@
-# # PRINT 1 TO 10
-# i=1
-# while(i<=10):
-#     print(i)
-#     i+=1
-# # print(i)
-# input("enter")
+# PRINT 1 TO 10
+i=1
+while(i<=10):
+    print(i)
+    i+=1
+# print(i)
+input("enter")
 
-# i=5
-# while(i<=0):
-#     print(i)
-#     i*1=i
-# input("enter")
+i=5
+while(i<=0):
+    print(i)
+    i*1=i
+input("enter")
