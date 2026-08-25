6ea10d3bc0f8e170f42703b0f98832924626bba1
SHIVRAJ.A.MOHITE <154596032+Shivrajmohite7@users.noreply.github.com>
2026-08-26 00:21:41 +0530

    Add files via upload

--- FBS/CORE PYTHON/DEMOS/BRANCHING/nested_if_else2.py
+++ FBS/CORE PYTHON/DEMOS/BRANCHING/nested_if_else2.py
@@ -1,18 +1,18 @@
 
 # 2.VOTING ELGIBILITY BY AGE AND CITIZEN CHECK ALSO.
 
-# Age=int(input("Enter The Age:"))
-# Citizen=input(f"Enter in 'yes/no':")
+Age=int(input("Enter The Age:"))
+Citizen=input(f"Enter in 'yes/no':")
 
-# if Age>17:
-#     if Citizen=='yes':
-#         print("CAN VOTE NOW")
+if Age>17:
+    if Citizen=='yes':
+        print("CAN VOTE NOW")
 
-#     else:{
-#         print("CANNOT VOTE")
-#     }
-# else:{
-#     print("AGE WRONG")
-# }
+    else:{
+        print("CANNOT VOTE")
+    }
+else:{
+    print("AGE WRONG")
+}
 
 # input("enter")
--- FBS/CORE PYTHON/DEMOS/BRANCHING/nested_if_else3.py
+++ FBS/CORE PYTHON/DEMOS/BRANCHING/nested_if_else3.py
@@ -1,16 +1,16 @@
 
 # 3.LOGIN SYSTEM
 
-# username=(input("Enter Username:"))
-# password=int(input("Enter Password:"))
+username=(input("Enter Username:"))
+password=int(input("Enter Password:"))
 
-# if username=='admin123':
-#     if password==1234:
-#         print("Password verified")
+if username=='admin123':
+    if password==1234:
+        print("Password verified")
     
-#     else:
-#         print("Something went wrong")
-# else:
-#     print("Invalid username")  
+    else:
+        print("Something went wrong")
+else:
+    print("Invalid username")  
 
 # input("enter")
