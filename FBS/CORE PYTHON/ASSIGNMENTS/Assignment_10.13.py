659a9e1caeab760f411df106e8aa052f724bd969
SHIVRAJ.A.MOHITE <154596032+Shivrajmohite7@users.noreply.github.com>
2026-08-30 23:19:26 +0530

    Add files via upload

---
+++ FBS/CORE PYTHON/ASSIGNMENTS/Assignment_10.13.py
@@ -0,0 +1,16 @@
+# 13 . Write a program to print list after removing even numbers.
+
+
+def getData(li):
+    i=0
+    while i<len(li):
+        if li[i]%2==0:
+            del li[i]
+        else:
+            i+=1
+
+    return li
+
+li=[10,20,30,1,3,5,7]
+result=getData(li)
+print(result)
